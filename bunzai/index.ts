27b1be61558c119c98ba481e40dfe7b25ed9712f
ariaandika <andikaakun17@gmail.com>
2024-07-21 14:50:39 +0700

    add bun program

---
+++ bunzai/index.ts
@@ -0,0 +1,40 @@
+
+function getPort() {
+    const ports = Bun.spawnSync({
+        cmd: ["ss","-tln"],
+        stdout: "pipe",
+    })
+        .stdout
+        .toString()
+        .split("\n")
+        .filter(e => e.startsWith("LISTEN"))
+        .map(e => e.split(/\s+/)[3])
+        .filter(e => e && e.match(/40\d\d$/))
+        .map(e => parseInt(e.slice(-4)))
+
+    for (let i = 4000;i <= 4020;i++) {
+        if (!ports.includes(i)) {
+            return i
+        }
+    }
+
+    console.error("No Port Available")
+    process.exit(1);
+}
+
+
+
+
+const tcp = Bun.listen({
+    hostname: "127.0.0.1",
+    port: getPort(),
+    socket: {
+        data(socket, data) {
+
+        },
+    },
+})
+
+console.log(`Listening in ${tcp.hostname}:${tcp.port}`)
+
+
