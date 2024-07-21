b459e9e2142381266ddebeb6be82eb3abc3e9744
ariaandika <andikaakun17@gmail.com>
2024-07-21 19:03:01 +0700

    update ruster: use addr instead of port

--- ruster/src/main.rs
+++ ruster/src/main.rs
@@ -1,13 +1,15 @@
-use std::collections::HashSet;
 use std::io::{self, BufRead, BufReader, Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
 use std::process::{Command, Stdio};
 use std::sync::mpsc::SyncSender;
 use std::thread::sleep;
 use std::time::Duration;
 use std::{sync, thread};
 
-fn get_addr() -> io::Result<String> {
+const PORTS: std::ops::Range<u16> = 4000..4020;
+const HOST: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+
+fn get_addr() -> io::Result<SocketAddr> {
     let ss = Command::new("ss")
         .arg("-tln")
         .stdout(Stdio::piped())
@@ -16,30 +18,43 @@ fn get_addr() -> io::Result<String> {
     let mut buf = String::new();
     let _ = ss.stdout.expect("piped").read_to_string(&mut buf)?;
 
-    let bound_ports = buf
-        .lines()
-        .filter(|e| e.starts_with("LISTEN"))
+    let mut addrs = buf.lines()
         .filter_map(|e| {
-            e.split_whitespace()
-                .nth(3)?
-                .get("127.0.0.1:".len()..)?
-                .parse::<u64>()
-                .ok()
-        }).collect::<HashSet<_>>();
+            e.starts_with("LISTEN").then_some(())?;
 
-    let port = (4000u64..4020)
-        .find(|e|!bound_ports.contains(e))
-        .ok_or(io::Error::new(io::ErrorKind::AddrInUse, "No Port Avilable"))?;
+            let addr = e.split_whitespace().nth(3)
+                .map(str::parse::<SocketAddr>)?.ok()?;
 
-    Ok(format!("127.0.0.1:{port}"))
+            (addr.ip() == HOST && PORTS.contains(&addr.port())).then_some(addr)
+        })
+        .collect::<Vec<_>>();
+
+    if addrs.is_empty() {
+        return Ok(SocketAddr::new(HOST, 4000));
+    }
+
+    addrs.sort_by(|a,b|a.port().partial_cmp(&b.port()).expect("port cannot be NaN"));
+
+    let mut ports = PORTS.clone();
+
+    for (addr, port) in addrs.into_iter().zip(&mut ports) {
+        if port != addr.port() {
+            return Ok(SocketAddr::new(HOST, port));
+        }
+    }
+
+    match ports.next() {
+        Some(port) => Ok(SocketAddr::new(HOST, port)),
+        None => Err(io::Error::new(io::ErrorKind::AddrInUse, "No Port Available")),
+    }
 }
 
 fn main() -> io::Result<()> {
-    let tcp = TcpListener::bind(get_addr()?)?;
+    let tcp = TcpListener::bind(dbg!(get_addr()?))?;
     let (tx,_) = sync::mpsc::sync_channel::<String>(0);
     let pool = 0u8;
 
-    println!("Listening {}",tcp.local_addr()?);
+    println!("[Ruster] Listening {}",tcp.local_addr()?);
 
     for stream in tcp.incoming() {
         let stream = match stream {
@@ -54,7 +69,7 @@ fn main() -> io::Result<()> {
             let tx = tx.clone();
             thread::spawn(||handle(stream, tx));
         } else {
-            println!("Busy");
+            println!("[TODO] Busy");
         }
     }
 
