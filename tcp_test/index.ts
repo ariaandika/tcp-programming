1152fed7a83d6885190793e005f8817e048cf50b
ariaandika <andikaakun17@gmail.com>
2024-07-21 16:20:35 +0700

    add tcp testing port as param

--- tcp_test/index.ts
+++ tcp_test/index.ts
@@ -1,10 +1,19 @@
+let port = parseInt(process.argv[2]);
+
+if (!port || isNaN(port)) {
+    console.error("Port required")
+    process.exit(1);
+}
 
 const socket = await Bun.connect({
-    port: 4000,
+    port,
     hostname: "127.0.0.1",
     socket: {
-        data(socket, data) {
+        data(_, data) {
             console.log("Then",data.toString());
+        },
+        close() {
+            console.log("Tcp Closed")
         },
     }
 })
